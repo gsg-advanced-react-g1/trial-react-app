e209365628c2f872f0a47ddb543cf1187804cb52
Belal-sk99 <belalasem19991@gmail.com>
2026-01-05 23:03:46 +0200

    create ProductsContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { ProductsProvider } from './modules/Products';
 
 function App() {
 
   return (
     <MantineProvider>
-      <h1> This is trial react app </h1>
+      <ProductsProvider value="Products">
+        <h1> This is trial react app </h1>
+      </ProductsProvider>
     </MantineProvider>
   )
 }
