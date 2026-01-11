5e2c4ff1c043593a6643b35032bbce84b19cebd7
mFlaifel <msf-2oo7@hotmail.com>
2026-01-11 13:32:57 +0200

    install and setup react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import './App.css';
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
-import Products from './modules/Products/views';
+// import Products from './modules/Products/views';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { createProductsModule } from './modules/Products/index.tsx';
+import { Products } from './modules/Products/views/index.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const { Provider: ProductsProvider } = createProductsModule();
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <MantineProvider>
-      <ProductsProvider>
-        <h3> This is trial react app </h3>
-        <Products />
-      </ProductsProvider>
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <ProductsProvider>
+          <ReactQueryDevtools />
+          <h3> This is trial react app </h3>
+          <Products />
+        </ProductsProvider>
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }
 
