491c321765f5bf299579d82f028f2993c25c58b7
Belal-sk99 <belalasem19991@gmail.com>
2026-01-21 20:38:22 +0200

    create/ flags provider and config file

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./modules/Theme/index.tsx";
+import { FeatureFlagProvider } from "./modules/FeatureFlags/index.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
-  </StrictMode>
-);
+const start = () => {
+
+  fetch("./config.json").then((response) => response.json()).then((config) => {
+    const root = createRoot(document.getElementById("root")!);
+    root.render(
+      <StrictMode>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <FeatureFlagProvider value={config}>
+            <App />
+          </FeatureFlagProvider>
+        </ThemeProvider>
+      </StrictMode>
+    );
+  })
+}
+
+start();
