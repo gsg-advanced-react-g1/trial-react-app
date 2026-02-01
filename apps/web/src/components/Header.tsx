678438c84ac2920a15f0b794c63e3f1fd5dbd99d
mFlaifel <msf-2oo7@hotmail.com>
2026-02-01 14:03:39 +0200

    apply design system to a button

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,45 +1,43 @@
-import { Button, Group } from "@mantine/core";
-import { Link } from "@tanstack/react-router";
-import { ModeToggleBtn } from "../modules/Theme/Views/ModeToggleBtn";
-import { IconBuildingStore } from "@tabler/icons-react";
-import { LogoutButton } from "../modules/Auth/views/logout";
-import { useAuth } from "../modules/Auth/hooks/useAuth";
+import { Group } from '@mantine/core';
+import { Link } from '@tanstack/react-router';
+import { ModeToggleBtn } from '../modules/Theme/Views/ModeToggleBtn';
+import { IconBuildingStore } from '@tabler/icons-react';
+import { LogoutButton } from '../modules/Auth/views/logout';
+import { useAuth } from '../modules/Auth/hooks/useAuth';
+import { Button } from '@advanced-react/ui';
 
 const navs = [
   {
-    label: "Home",
-    href: "/",
+    label: 'Home',
+    href: '/',
   },
   {
-    label: "Products",
-    href: "/products",
+    label: 'Products',
+    href: '/products',
   },
 ];
 
 const Header = () => {
-
   const { user } = useAuth();
 
   return (
-    <header
-      className="sticky top-0 left-0 right-0 z-50 mb-3 flex items-center justify-between rounded-lg p-3 shadow-xl backdrop-blur-lg " >
-
-      <Link to="/" className="flex items-center">
-        <span className="font-bold text-lg flex items-center gap-2 cursor-pointer">
+    <header className='sticky top-0 left-0 right-0 z-50 mb-3 flex items-center justify-between rounded-lg p-3 shadow-xl backdrop-blur-lg '>
+      <Link to='/' className='flex items-center'>
+        <span className='font-bold text-lg flex items-center gap-2 cursor-pointer'>
           <IconBuildingStore size={24} />
           Ecommerce
         </span>
       </Link>
-      <div className="absolute left-1/2 transform -translate-x-1/2">
-        <Group className="flex items-center justify-center gap-8">
+      <div className='absolute left-1/2 transform -translate-x-1/2'>
+        <Group className='flex items-center justify-center gap-8'>
           {navs.map((nav) => {
             return (
               <Link
                 key={nav.label}
                 to={nav.href}
-                className="hover:text-black dark:hover:text-white transition-all duration-300"
-                activeProps={{ className: "border-b-1" }}
-                inactiveProps={{ className: "" }}
+                className='hover:text-black dark:hover:text-white transition-all duration-300'
+                activeProps={{ className: 'border-b-1' }}
+                inactiveProps={{ className: '' }}
               >
                 {nav.label}
               </Link>
@@ -47,14 +45,16 @@ const Header = () => {
           })}
         </Group>
       </div>
-      <div className="flex items-center gap-5">
+      <div className='flex items-center gap-5'>
         <ModeToggleBtn />
-        {user ? <LogoutButton variant="outline" /> :
-          <div className="flex items-center gap-3">
-            <Button variant="outline" component={Link} to="/login">Login</Button>
-            <Button variant="filled" component={Link} to="/register">Register</Button>
+        {user ? (
+          <LogoutButton variant='outline' />
+        ) : (
+          <div className='flex items-center gap-3'>
+            <Button variant='secondary' label='Login' />
+            <Button variant='primary' label='Register' />
           </div>
-        }
+        )}
       </div>
     </header>
   );
