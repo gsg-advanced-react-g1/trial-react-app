7fad52e5ec1d1b7b061c653ab9911df5d2ae7eca
Belal-sk99 <belalasem19991@gmail.com>
2026-02-01 13:42:27 +0200

    create design system - initial step

---
+++ packages/ui/src/types/Color.ts
@@ -0,0 +1,12 @@
+export type Color =
+    | 'initial'
+    | 'fg-primary'
+    | 'fg-secondary'
+    | 'brand-primary'
+    | 'brand-secondary'
+    | 'bg-primary'
+    | 'success'
+    | 'error'
+    | 'border'
+    | 'white'
+    | 'blue';
---
+++ packages/ui/src/types/Spacing.ts
@@ -0,0 +1,1 @@
+export type Spacing = "xxs" | "xs" | "sm" | "md" | "lg" | "xl";
